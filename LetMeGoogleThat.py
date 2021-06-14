2e024e73ccb6baa69dd8c4dbe743ec3f00e9e41a
Mister-Three <81454774+Mister-Three@users.noreply.github.com>
2021-06-14 14:26:33 -0400

    Add files via upload
    To annoy people

---
+++ LetMeGoogleThat.py
@@ -0,0 +1,22 @@
+import pyperclip as pc
+import webbrowser
+import time
+lmgt = input("Input the Let Me Google That Phrase: \n")
+lmgt.replace(" ", "+")
+link = ('https://letmegooglethat.com/?q=' + lmgt)
+ask = input("Would you like to open the Let Me Google That link? \n")
+if ask.lower() in ['yes', 'y']:
+    webbrowser.open(link)
+    print("Link has been opened...")
+    time.sleep(2)
+    print("Closing this window...")
+    exit()
+elif ask.lower() in ['no', 'n']:
+    pc.copy(link)
+    print("Link has been copied...")
+    time.sleep(2)
+    print("Closing window in 5 seconds...")
+    time.sleep(5)
+    exit('easy')
+else:
+    print("That's not a valid statment!")
