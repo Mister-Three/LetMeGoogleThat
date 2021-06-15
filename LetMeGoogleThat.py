59700f85bccba7779a89b2ab73fe11b78b7bd122
Mister-Three <81454774+Mister-Three@users.noreply.github.com>
2021-06-15 16:21:23 -0400

    Update LetMeGoogleThat.py

--- LetMeGoogleThat.py
+++ LetMeGoogleThat.py
@@ -1,10 +1,9 @@
 import pyperclip as pc
 import webbrowser
 import time
-lmgt = input("Input the Let Me Google That Phrase: \n")
-lmgt.replace("", "+")
+lmgt = input("Input the Let Me Google That Phrase: ")
 link = ('https://letmegooglethat.com/?q=' + lmgt)
-ask = input("Would you like to open the Let Me Google That link?[Y/N]: ")
+ask = input("Would you like to open the Let Me Google That link? [Y/N]: ")
 if ask.lower() in ['yes', 'y']:
     webbrowser.open(link)
     print("Link has been opened...")
@@ -17,6 +16,6 @@ elif ask.lower() in ['no', 'n']:
     time.sleep(2)
     print("Closing window in 5 seconds...")
     time.sleep(5)
-    exit('easy')
+    exit()
 else:
     print("That's not a valid statment!")
